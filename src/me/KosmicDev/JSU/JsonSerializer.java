38e108bae4965fb041e8f20b6a6e641f38677612
Matt <39469614+KosmicShovel@users.noreply.github.com>
2020-12-05 06:09:54 -0600

    Raw Text -> Json Text

--- src/me/KosmicDev/JSU/JsonSerializer.java
+++ src/me/KosmicDev/JSU/JsonSerializer.java
@@ -2,21 +2,24 @@ package me.KosmicDev.JSU;
 
 import com.sun.istack.internal.Nullable;
 
+import java.io.Console;
 import java.lang.reflect.Field;
 import java.util.*;
 
 public class JsonSerializer {
 
     public static String Serialize(Object value) {
-        String result = "";
+        String result = "{\n";
 
         Class<?> clazz = value.getClass();
 
         for (Field f : clazz.getFields()) {
             try {
-                result += f.getName() + " : " + f.get(value) + " : " + f.get(value).getClass().getTypeName() + "\n";
+                result += "\t\"" + f.getName() + "\": \"" + f.get(value) + "::" + f.get(value).getClass().getTypeName() + "\"" + (clazz.getFields()[clazz.getFields().length - 1].equals(f) ? "" : ",") + "\n";
             } catch (Exception e) {}
         }
+
+        result += "}";
 
         return result;
     }
@@ -25,11 +28,12 @@ public class JsonSerializer {
         try {
             Object clazz = type.newInstance();
             HashMap<String, Object> FieldValues = new HashMap<>();
-            for (String line : json.split("\n")) {
-                String[] parts = line.split(" : ");
-                String name = parts[0];
-                String value = parts[1];
-                String typeName = parts[2];
+            for (String line : trimExcess(json).split("\n")) {
+                String[] parts = line.split(": ");
+                String name = parts[0].replace("\"", "");
+                String[] parts2 = parts[1].split("::");
+                String value = parts2[0].replace("\"", "");
+                String typeName = parts2[1].replace("\"", "").replace(",", "");
                 FieldValues.put(name, Converter.convert(Class.forName(typeName), value));
             }
 
@@ -42,5 +46,9 @@ public class JsonSerializer {
             System.out.println(e);
         }
         return null;
+    }
+
+    private static String trimExcess(String json) {
+        return json.replace("{\n", "").replace("}", "").replace("\t", "");
     }
 }
--- src/me/KosmicDev/JSU/Main.java
+++ src/me/KosmicDev/JSU/Main.java
@@ -1,8 +1,13 @@
 package me.KosmicDev.JSU;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
 
     public static void main(String... args) {
+        List<Float> timeTaken = new ArrayList<>();
+
         TempClass o = new TempClass();
         o.anInt = 87;
         o.aBoolean = true;
@@ -10,14 +15,21 @@ public class Main {
         o.arrayList.add("Test2");
         o.arrayList.add(54);
         o.arrayList.add('c');
-        String s = JsonSerializer.Serialize(o);
-        System.out.println(s);
-        TempClass deserializedClass = (TempClass) JsonSerializer.Deserialize(TempClass.class, s,null);
-        System.out.println(o.anInt); //87
-        System.out.println(o.aBoolean); //true
-        System.out.println(o.arrayList.get(0)); //Test
-        System.out.println(o.arrayList.get(1)); //Test2
-        System.out.println(o.arrayList.get(2)); //54
-        System.out.println(o.arrayList.get(3)); //c
+
+        for (int i = 0; i < 500; i++) {
+            long timeThen = System.nanoTime();
+            String gcThis = JsonSerializer.Serialize(o);
+            long timeNow = System.nanoTime();
+            gcThis = null;
+            System.gc();
+            float timeTook = (timeNow - timeThen) / 1000000f;
+            timeTaken.add(timeTook);
+            System.out.println("Time taken was " + (timeTook > 0.02f ? "\u001b[31m" : (timeTook < 0.01f ? "\u001b[32m" : "\u001b[33m")) + timeTook + "ms\u001b[00m.");
+            System.out.println("---------------------------------------------------------");
+        }
+
+        //Average Time Taken was 0.002ms.
+        //Usually ranges from 0.004ms to 0.002ms
+        System.out.println("Average Time Taken was \u001b[36m" + timeTaken.stream().mapToDouble(a -> Math.round(a * 100)/100).average().getAsDouble() + "ms\u001b[00m.");
     }
 }
